cef58609935104320fb9c6be59194866c906c77d
q-werty452 <qwerty226179@gmail.com>
2026-02-10 13:19:14 +0600

    news/urls.py

--- apps/news/urls.py
+++ apps/news/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from apps.news.views import homepage, news_detail, category_detail, search
+from apps.news.views import homepage, news_detail, category_detail, search, profile, registr
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [ 
@@ -7,4 +8,12 @@ urlpatterns = [
     path('search/', search, name='search'), 
     path('news/<slug:slug>/', news_detail, name='news_detail'), 
     path('category/<slug:slug>/', category_detail, name='category_detail'),
+
+    path('profile/', profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='pages/login.html'), name='login'),
+
+
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('registr/', registr, name='registr'),
+
 ]
